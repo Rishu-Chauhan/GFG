12475bbfd012d2a5d37a3b635b30ca371565567d
Rishu-Chauhan <rishuchauhan798@gmail.com>
2025-03-15 10:36:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stickler Thief/stickler-thief.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            String inputLine[] = br.readLine().trim().split(" ");
+            int n = inputLine.length;
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            // calling method findMaxSum() of class solve
+            System.out.println(new Solution().findMaxSum(arr));
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to find the maximum money the thief can get.
+    public int findMaxSum(int arr[]) {
+        int n=arr.length;
+        if(n<2){
+            return arr[0];
+        }
+        int dp[]=new int[n];
+        dp[0]=arr[0];
+        dp[1]=Math.max(arr[0],arr[1]);
+        for(int i=2;i<n;i++){
+            dp[i]=Math.max(dp[i-2]+arr[i],dp[i-1]);
+        }
+        return dp[n-1];
+    }
+}
