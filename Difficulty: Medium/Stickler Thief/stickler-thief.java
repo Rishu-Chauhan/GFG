9951d6c61870062ffdfd4f13d9bd7786377f9d09
Rishu-Chauhan <rishuchauhan798@gmail.com>
2025-03-21 08:09:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stickler Thief/stickler-thief.java
+++ Difficulty: Medium/Stickler Thief/stickler-thief.java
@@ -25,17 +25,16 @@ class GFG {
 
 
 class Solution {
-    // Function to find the maximum money the thief can get.
     public int findMaxSum(int arr[]) {
         int n=arr.length;
         if(n<2){
-            return arr[0];
-        }
-        int dp[]=new int[n];
-        dp[0]=arr[0];
-        dp[1]=Math.max(arr[0],arr[1]);
-        for(int i=2;i<n;i++){
-            dp[i]=Math.max(dp[i-2]+arr[i],dp[i-1]);
+          return arr[0];
+            }
+          int dp[]=new int[n];
+            dp[0]=arr[0];
+            dp[1]=Math.max(arr[0],arr[1]);
+            for(int i=2;i<n;i++){
+                dp[i]=Math.max(dp[i-2]+arr[i],dp[i-1]);
         }
         return dp[n-1];
     }
