adeaf71f31b0d0aed1583fc8674db5e5603a1f32
Rishu-Chauhan <rishuchauhan798@gmail.com>
2025-05-01 09:31:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Pascal Triangle/pascal-triangle.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+
+            ArrayList<Integer> ans = new Solution().nthRowOfPascalTriangle(n);
+            printAns(ans);
+
+            System.out.println("~");
+        }
+    }
+
+    public static void printAns(ArrayList<Integer> ans) {
+        for (Integer x : ans) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+
+    ArrayList<Integer> nthRowOfPascalTriangle(int n) {
+        // code here
+         ArrayList<Integer> row = new ArrayList<>();
+        
+        long value = 1; 
+        row.add((int)value);
+        
+        for (int i = 1; i < n; i++) {
+            value = value * (n - i) / i;
+            row.add((int)value);
+        }
+        
+        return row;
+    }
+}
