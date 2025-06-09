22c88dbda220d2966652b2d6204a7204de2267a4
Rishu-Chauhan <rishuchauhan798@gmail.com>
2025-06-09 23:55:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Height of Binary Tree/height-of-binary-tree.java
@@ -0,0 +1,21 @@
+/*
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+ */
+
+class Solution {
+    // Function to find the height of a binary tree.
+    int height(Node node) {
+        // code here
+         return node == null ? -1 : 1 + Math.max(height(node.left), height(node.right));
+    }
+}
